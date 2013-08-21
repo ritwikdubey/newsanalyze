05eaaaaa7f157002b40f9dcf55f5caece254d489
Ritwik Dubey <ritwik.exe@gmail.com>
2013-08-21 05:25:32 +0000

    mongo.db support

--- market-research.js
+++ market-research.js
@@ -1,91 +1,29 @@
 #!/usr/bin/env node
-/*
-Use the Yahoo Finance CSV API to do some basic market research calculations.
 
- - Background: http://greenido.wordpress.com/2009/12/22/yahoo-finance-hidden-api/
- - Example URL: http://finance.yahoo.com/d/quotes.csv?s=GOOG+FB+AAPL&f=snj1pr
-    s:  Symbol
-    n:  Name
-    j1: Market Capitalization (in billions)
-    p:  Price-per-share (at previous close)
-    r:  Price to Earnings Ratio
-
-Further references.
-
- - https://github.com/danwrong/restler
- - https://github.com/wdavidw/node-csv
- - http://josscrowcroft.github.io/accounting.js
- - http://stackoverflow.com/questions/4981891/node-js-equivalent-of-pythons-if-name-main
- - http://nodejs.org/docs/latest/api/util.html#util_util_format_format
-
-*/
+var mongotest = require('./mongo-test');
+var MongoClient = require('mongodb').MongoClient
+, format = require('util').format;
 var AlchemyAPI = require('alchemy-api');
 var request = require("request");
 var cheerio = require("cheerio");
 var jsdom = require('jsdom');
 var util = require('util');
 var fs = require('fs');
-var rest = require('restler');
-var csv = require('csv');
-var accounting = require('accounting');
-var CSVFILE_DEFAULT = "market-research.csv";
-var SYMBOLS_DEFAULT = ["GOOG", "FB", "AAPL", "YHOO", "MSFT", "LNKD", "CRM"];
-var COLUMNS_DEFAULT = 'snj1pr'; // http://greenido.wordpress.com/2009/12/22/yahoo-finance-hidden-api
-var HEADERS_DEFAULT = ["Symbol", "Name", "Market Cap", "Previous Close Price",
-                       "P/E Ratio", "Shares", "EPS", "Earnings"];
 
-var financeurl = function(symbols, columns) {
-    return util.format(
-        'http://finance.yahoo.com/d/quotes.csv?s=%s&f=%s',
-        symbols.join('+'),
-        columns);
-};
-
-var marketCapFloat = function(marketCapString) {
-    return parseFloat(marketCapString.split('B')[0]) * 1e9;
-};
-
-var csv2console = function(csvfile, headers) {
-    console.log(headers.join("\t"));
-    csv()
-    .from.path(csvfile)
-    .on('record', function(row, index) {
-        var shares = Math.round(marketCapFloat(row[2])/row[3], 0);
-        var eps = (row[3]/row[4]).toFixed(3);
-        var earnings = accounting.formatMoney(eps * shares);
-        outrow = row.concat([shares, eps, earnings]);
-        console.log(outrow.join("\t"));
+var MongoConnect = function(){
+    MongoClient.connect('mongodb://127.0.0.1:27017/test', function(err, db) {
+    if(err) throw err;
     });
 };
 
-var buildfn = function(csvfile, headers) {
-    var response2console = function(result, response) {
-        if (result instanceof Error) {
-            console.error('Error: ' + util.format(response.message));
-        } else {
-            console.error("Wrote %s", csvfile);
-            fs.writeFileSync(csvfile, result);
-            csv2console(csvfile, headers);
-        }
-    };
-    return response2console;
-};
-
-var marketResearch = function(symbols, columns, csvfile, headers) {
-    symbols = symbols || SYMBOLS_DEFAULT;
-    columns = columns || COLUMNS_DEFAULT;
-    csvfile = csvfile || CSVFILE_DEFAULT;
-    headers = headers || HEADERS_DEFAULT;
-    var apiurl = financeurl(symbols, columns);
-    var response2console = buildfn(csvfile, headers);
-    rest.get(apiurl).on('complete', response2console);
-};
-
-var printtext = function(result){
-	console.log(result);
-	//jsdom.env(
-	  
-	//);
+var SaveKeywords = function(keywords,link, category){
+    console.log("in savekeywords");
+    var entry = {};
+    entry.link = link;
+    entry.category = category;
+    entry.keyowrds = keywords;
+    console.log(JSON.stringify(entry));
+    //mongotest.AddToDb(entry);
 };
 
 if(require.main == module) {
@@ -96,45 +34,46 @@ if(require.main == module) {
     } else {
         symbols = undefined;
     }
-    //marketResearch(symbols);
-    //var url = "http://archives.ndtv.com/articles/2013-07.html";
-    //rest.get(url).on('complete',function(result){
-	//printtext(result);	
-    //});
-	
+	MongoConnect();
+
 	request({
 	  uri: "http://archives.ndtv.com/articles/2013-07.html",
 	}, function(error, response, body) {
 	  var $ = cheerio.load(body);
-	    //var flag = false;
 	  console.log($("#main-content").find("li").length);
 	  $("#main-content").find("li").each(function() {
 	    var link = $(this);
 	    var text = link.text();
-	    //var lang = link.lang();
-		//console.log(text);
-//	    if(text === 'Helicopter lands in New York river, all safe'){
-		var flag = true;
-		//continue;
-		//}
-	    if(flag === true){
+		//var flag = true;
+	    //if(flag === true){
 	    var href = link.attr("href");
 	    //console.log(text + " -> " + href);
-	var alchemy = new AlchemyAPI('5bbb93b60d50ac7495f0333770c6963114c676f0');
-	link.find("a").each(function(){
+	    var isEng = true;
+		if (!text.match('/^[a-zA-Z \.\!\?]*$/')) {
+		    isEng = false;
+		    console.log(isEng + ": is here");
+		}	
+	   if(isEng){
+	   var alchemy = new AlchemyAPI('5bbb93b60d50ac7495f0333770c6963114c676f0');
+	   link.find("a").each(function(){
 	   var href1=  $(this).attr("href");
-	    alchemy.category(href1, {}, function(error, response){
-		var concept = response.category;
-		console.log(concept);
-});
-});
+	    var category;
 
-	//alchemy.concepts(href, {}, function(error, response){
-	//var senti = response.docSentiment;
-	//console.log(text);
-	//console.log(response.concepts);
-//});
-	}
+	    alchemy.category(href1, {}, function(error, response){
+		if(!error){
+		category = response.category;
+		    }
+	    });
+
+	    alchemy.keywords(href1, {}, function(error, response){
+		var keywords = response.keywords;
+		console.log(keywords);
+		SaveKeywords(keywords,href1,category);
+	    });
+
+	   });
+	  }
+	//}
 	    
 	  });
 	});
@@ -143,5 +82,5 @@ if(require.main == module) {
     console.error('Invoked via library call');
 }
 
-exports.marketResearch = marketResearch;
+
 
---
+++ mongo-test.js
@@ -0,0 +1,24 @@
+
+var collections = ['newsdata'];
+var dataconnection = 'localhost/mongoapp';
+var db = require('mongojs').connect(dataconnection, collections);
+
+function metadata(link, keywords, relevance, concept){
+    this.text = keywords;
+    this.relevance = relevance;
+    this.link = link;
+    this.concept = concept;
+}
+
+//var md1 = {"link": "href", "text" : "keyword1", "relevance": ".937"};
+
+var AddToDb = function(md1){
+//console.log("In AddToDb: "+ md1.text);
+db.newsdata.save(md1,function(err, saveUser){
+    if(!err){
+	console.log(saveUser.link + "-> saved");
+    }
+});
+}
+//AddToDb(md1);
+exports.AddToDb = AddToDb;
